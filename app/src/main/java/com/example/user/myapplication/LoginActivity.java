a069b8244b195be57ea241cd0946f66e7b4c7f30
hsin0603 <gemini880603@gmail.com>
2018-11-13 15:34:19 +0800

    getSharePreference

--- app/src/main/java/com/example/user/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/user/myapplication/LoginActivity.java
@@ -1,23 +1,34 @@
 package com.example.user.myapplication;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  LoginActivity extends AppCompatActivity {
+
+    private String userid;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        EditText Usesrid = findViewById(R.id.ed_userid);
+        String userid =getSharedPreferences("atm",MODE_PRIVATE).getString("userid","");
+        Usesrid.setText(userid);
     }
 
     public void login(View view){
-        String userid = ((EditText)findViewById(R.id.ed_userid)).getText().toString();
-        String password = ((EditText)findViewById(R.id.ed_password)).getText().toString();
+        userid = ((EditText)findViewById(R.id.ed_userid)).getText().toString();
+        password = ((EditText)findViewById(R.id.ed_password)).getText().toString();
         if ("june".equals(userid) && "8863".equals(password)){
-            setResult(RESULT_OK);
+          setResult(RESULT_OK);
+            getSharedPreferences("atm",MODE_PRIVATE)
+                    .edit()
+                    .putString("userid", userid)
+                    .apply();
             finish();
         }else {
             setResult(RESULT_CANCELED);
